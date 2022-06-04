ed1adbf370f1e2eac1da18e10ad7b36559e0e12b
raburuz <ratomlabs@gmail.com>
2022-06-04 18:28:58 +0200

    New API TOKEN, revalidate frontend x_token

---
+++ controllers/token.api.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import { signJWT } from '../helpers/jsonWebToken';
+import { UserData } from '../interfaces/user';
+import { UserModel } from '../models/user';
+
+export const revalidateToken = async (req: Request, res: Response) => {
+  const currentUser = req.currentUserId;
+
+  try {
+    const user: UserData | null = await UserModel.findById<UserData>(
+      currentUser
+    );
+    if (user) {
+      const { id, email, username, role } = user;
+      const token = await signJWT(id!);
+
+      return res.status(200).json({
+        ok: true,
+        msg: 'Logging Success',
+        user: {
+          id,
+          email,
+          username,
+          role,
+        },
+        token,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      msg: 'Sorry something was wrong, please contact with Admin',
+    });
+  }
+};
